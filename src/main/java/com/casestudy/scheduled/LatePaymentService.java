7b9eadd7e056adfda19f1e088e83052376ad43e2
mmsuerkan <mmsuerkan@gmail.com>
2023-08-27 18:30:23 +0300

    comments are deleted.

--- src/main/java/com/casestudy/scheduled/LatePaymentService.java
+++ src/main/java/com/casestudy/scheduled/LatePaymentService.java
@@ -8,15 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class LatePaymentService {
-    //kredilerin taksitleri gecikme faizi eklenerek güncellenir
 
     private final CreditService creditService;
     private final InstallmentService installmentService;
 
-    //her gün kontrol edilerek, son ödeme tarihi geçmiş taksitlerin faizleri hesaplanır ve güncellenir
+
     final void calculateLatePaymentInterest(Double interestRate) {
 
-        //kredilerin taksitleri gecikme faizi eklenerek güncellenir
         creditService.findAllCredits().forEach(credit -> {
             installmentService.findAllByCreditId(Long.valueOf(credit.getId())).forEach(installment -> {
                 installmentService.calculateLatePaymentInterest(installment,interestRate);
